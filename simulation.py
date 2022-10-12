b1f01e64d80e0916637c2c267f047def36246392
louisljamie <louis.l.jamie@gmail.com>
2022-10-12 11:09:01 -0500

    Finished v.5

---
+++ simulation.py
@@ -0,0 +1,41 @@
+# How likely is it that you roll doubles when rolling two dice?
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%config InlineBackend.figure_format = 'retina'
+import viz # curriculum example visualizations
+
+np.random.seed(29)
+
+
+# There are 3 web development cohorts, for every 1 data science cohort at Codeup.
+# Codeup randomly selects one to put on a billboard,
+# what are the odds that the two billboards?
+
+
+
+
+
+
+
+# Codeup students buy, on average, 3 poptart packages with a standard deviation of 1.5 a day from the snack vending machine.
+# If on monday the machine is restocked with 17 poptart packages, how likely is it that I will be able to buy some poptarts
+# on Friday afternoon? (Remember, if you have mean and standard deviation, use the np.random.normal) You’ll need to make a
+# judgement call on how to handle some of your values.
+
+
+Compare Heights
+Men have an average height of 178 cm and standard deviation of 8cm.
+Women have a mean of 170, sd = 6cm.
+Since you have means and standard deviations, you can use np.random.normal to generate observations.
+If a man and woman are chosen at random, what is the likelihood the woman is taller than the man?
+When installing anaconda on a student’s computer, there’s a 1 in 250 chance that the download is corrupted and the installation fails.
+What are the odds that after having 50 students download anaconda, no one has an installation issue? 100 students?
+What is the probability that we observe an installation issue within the first 150 students that download anaconda?
+How likely is it that 450 students all download anaconda without an issue?
+There’s a 70% chance on any given day that there will be at least one food truck at Travis Park.
+However, you haven’t seen a food truck there in 3 days. How unlikely is this?
+How likely is it that a food truck will show up sometime this week?
+If 23 people are in the same room, what are the odds that two of them share a birthday? What if it’s 20 people? 40 people?
+Be sure to add, commit, and push your work. Additional bonus exercises are available on the curriculum website.
